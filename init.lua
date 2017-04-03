5897f0f4c21c1171c8eb219778171e90e3ed2019
Steve Rubin <nerdforlife@gmail.com>
2017-04-03 10:27:02 -0700

    check by new device, not by new output

--- init.lua
+++ init.lua
@@ -1,19 +1,21 @@
 -- Avoid automatically setting a bluetooth audio input device
 
-lastSetOutputDeviceTime = os.time()
+lastSetDeviceTime = os.time()
 lastInputDevice = nil
 
 function audioDeviceChanged(arg)
-    if arg == 'dOut' then
-        lastSetOutputDeviceTime = os.time()
-    elseif arg == 'dIn ' and os.time() - lastSetOutputDeviceTime < 2 then
+    if arg == 'dev#' then
+        lastSetDeviceTime = os.time()
+    elseif arg == 'dIn ' and os.time() - lastSetDeviceTime < 2 then
         inputDevice = hs.audiodevice.defaultInputDevice()
         if inputDevice:transportType() == 'Bluetooth' then
             internalMic = lastInputDevice or hs.audiodevice.findInputByName('Built-in Microphone')
             internalMic:setDefaultInputDevice()
         end
     end
-    lastInputDevice = hs.audiodevice.defaultInputDevice()
+    if hs.audiodevice.defaultInputDevice():transportType() ~= 'Bluetooth' then
+        lastInputDevice = hs.audiodevice.defaultInputDevice()
+    end
 end
 
 hs.audiodevice.watcher.setCallback(audioDeviceChanged)
